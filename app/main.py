5bbf85e141454278658283e11eca3a0a34a4b14b
aki <aki@akis-MacBook-Air.local>
2023-08-15 14:57:24 +0900

    commit3

--- app/main.py
+++ app/main.py
@@ -26,108 +26,77 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!!!!!!'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+
+
+pdf_files = glob.glob("pdf_list/*.pdf")
 
 
 
+@app.route('/callback')
+def callback():
+    # 各PDFの全ページからデータを取得
+    pages_contents = ''
+    for filename in pdf_files:
+        # PDFを読み込み
+        pages_contents += read_pdf(filename)
+
+    chunks = pages_contents
+    print('URLのPDF群から情報を抽出しました。')
+    print(chunks)
+
+    text = chunks
+
+    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+    def count_tokens(text: str) -> int:
+        return len(tokenizer.encode(text))
+
+    # Split text into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 512,
+        chunk_overlap  = 24,
+        length_function = count_tokens,
+    )
+
+    chunks3 = text_splitter.create_documents([text])
+
+    print('step 222')
+    print(chunks3)
+
+    # DB利用
+
+    # Get embedding model
+    embeddings = OpenAIEmbeddings()
+
+    #  vector databaseの作成
+    db = FAISS.from_documents(chunks3, embeddings)
+
+    # query = "所有者とアクセス許可設定を元に戻すは何ページ目ですか？"
+    # query = "ローカルユーザーの共有フォルダーへのアクセスを制限する方法を教えてください"
+    # query = "共有フォルダーのデータを誤って消去しないためにはどうすればいい？"
+    # query = "所有者とアクセス許可設定を元に戻すは何ページ目ですか？"
+    # query = "Active Directoryドメインユーザーの共有フォルダーへのアクセスを制限するにはどうすればいいですか？"
+    query = "違反行為に対する抑止力の強化に関して何が改正されましたか？"
+    # query = "明日の天気はなんだと思いますか？情報を元に答えてください。"
+    query += "仮に判断できない場合は『該当する情報が見つかりませんでした。』と回答してください。"
+
+    embedding_vector = embeddings.embed_query(query)
+    docs_and_scores = db.similarity_search_by_vector(embedding_vector)
+
+    print('これがデータベースの中身です。')
+    print(docs_and_scores)
 
 
+    chain = load_qa_chain(OpenAI(temperature=0.1,max_tokens=1000), chain_type="stuff")
 
-
-
-
-
-
-
-
-
-
-
-
-
-# pdf_files = glob.glob("pdf_list/*.pdf")
-
-# # PDFの読み込みを行う関数
-# def read_pdf(filename):
-#     with open(filename, 'rb') as file:
-#         page_contents = ''
-#         reader = PdfReader(filename)
-#         number_of_pages = len(reader.pages)
-#         page_contents = ""
-#         for page_number in range(number_of_pages):
-#             pages = reader.pages
-#             page_contents += pages[page_number].extract_text()
-
-#         return page_contents
-
-
-
-# @app.route('/callback')
-# def callback():
-#     # 各PDFの全ページからデータを取得
-#     pages_contents = ''
-#     for filename in pdf_files:
-#         # PDFを読み込み
-#         pages_contents += read_pdf(filename)
-
-#     chunks = pages_contents
-#     print('URLのPDF群から情報を抽出しました。')
-#     print(chunks)
-
-#     text = chunks
-
-#     tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-
-#     def count_tokens(text: str) -> int:
-#         return len(tokenizer.encode(text))
-
-#     # Split text into chunks
-#     text_splitter = RecursiveCharacterTextSplitter(
-#         # Set a really small chunk size, just to show.
-#         chunk_size = 512,
-#         chunk_overlap  = 24,
-#         length_function = count_tokens,
-#     )
-
-#     chunks3 = text_splitter.create_documents([text])
-
-#     print('step 222')
-#     print(chunks3)
-
-#     # DB利用
-
-#     # Get embedding model
-#     embeddings = OpenAIEmbeddings()
-
-#     #  vector databaseの作成
-#     db = FAISS.from_documents(chunks3, embeddings)
-
-#     # query = "所有者とアクセス許可設定を元に戻すは何ページ目ですか？"
-#     # query = "ローカルユーザーの共有フォルダーへのアクセスを制限する方法を教えてください"
-#     # query = "共有フォルダーのデータを誤って消去しないためにはどうすればいい？"
-#     # query = "所有者とアクセス許可設定を元に戻すは何ページ目ですか？"
-#     # query = "Active Directoryドメインユーザーの共有フォルダーへのアクセスを制限するにはどうすればいいですか？"
-#     query = "違反行為に対する抑止力の強化に関して何が改正されましたか？"
-#     # query = "明日の天気はなんだと思いますか？情報を元に答えてください。"
-#     query += "仮に判断できない場合は『該当する情報が見つかりませんでした。』と回答してください。"
-
-#     embedding_vector = embeddings.embed_query(query)
-#     docs_and_scores = db.similarity_search_by_vector(embedding_vector)
-
-#     print('これがデータベースの中身です。')
-#     print(docs_and_scores)
-
-
-#     chain = load_qa_chain(OpenAI(temperature=0.1,max_tokens=1000), chain_type="stuff")
-
-#     message = chain.run(input_documents=docs_and_scores, question=query)
-#     print(query)
-#     print('に対する回答は以下の通りです。')
-#     print(message)
-#     return message
-#     # sys.exit()
-#     # ここまで
+    message = chain.run(input_documents=docs_and_scores, question=query)
+    print(query)
+    print('に対する回答は以下の通りです。')
+    print(message)
+    return message
+    # sys.exit()
+    # ここまで
 
 
 
@@ -142,3 +111,6 @@ if __name__ == "__main__":
     #     TextSendMessage(text=message)
     #     )
     # ここまで１段下げる
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
